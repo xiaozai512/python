0559f0cbf5ddd63cfdddff0c3ab61b3fd5d2d257
zekunwang <zekun.wang@tenbent.com>
2018-01-04 15:26:38 +0800

    create

---
+++ test.py
@@ -0,0 +1,15 @@
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@log('execute')
+def now():
+    print("2017-12-12")
+
+
+print(now())
